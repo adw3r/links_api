59e92f63316935da79d17f74cb3fe475d5b3ab4d
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-24 15:40:59 +0200

    fix

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -8,19 +8,21 @@ import config
 class TestEndpoint(TestCase):
 
     def test_get_link(self):
-        self.skipTest('deprecated')
         params = {
             'project_name': 'AllRight',
             'targets_base': 'turk',
+            'donor': 'testdonor'
         }
         url = f'http://localhost:{config.PORT}/link'
         resp = requests.get(url, params=params)
         self.assertTrue('bit.ly/' in resp.text)
 
     def test_get_link_v2(self):
+        self.skipTest('deprecated')
         params = {
             'project_name': 'AllRight',
             'targets_base': 'turk',
+            'donor': 'testdonor'
         }
         url = f'http://localhost:{config.PORT}/link_v2'
         resp = requests.get(url, params=params)
